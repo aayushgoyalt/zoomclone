c8dda5c0202f862bb6db77e7f3546c72444e137f
aayush <aayushgoyaldps@gmail.com>
2024-06-12 09:56:40 +0530

    Submmision

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,116 @@
+'use client'
+import React from 'react'
+import Image from 'next/image';
+import HomeCard from './HomeCard';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
+import { useUser } from '@clerk/nextjs';
+import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { Call } from '@stream-io/node-sdk';
+import { useToast } from "@/components/ui/use-toast"
+
+const MeetingTypeList = () => {
+  const router =  useRouter();
+  const [meetingState, setmeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+  const {user}=useUser();
+  const client=useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: '',
+})
+
+const [callDetails, setCallDetails] = useState<Call>();
+const { toast } = useToast();
+  
+  const CreateMeeting = async () =>{
+    if(!client || !user) return;
+
+    try {
+      if(!values.dateTime)  {toast({
+        title: "Please select a date and time",
+      });
+      return;}
+      const id = crypto.randomUUID();
+      const call = client.call('default',id);
+
+      if(!call) throw new Error('failed Call found');
+
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || '';
+      const link = values.link || '';
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          }
+        }
+      })      
+
+      setCallDetails(call);
+      if(!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+
+      toast({
+        title: "Meeting created",
+      })
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to Creaete Meeting",
+      });
+    }
+    
+  }
+
+
+  
+  return (
+    <section className=' grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+      <HomeCard
+      img='/icons/add-meeting.svg'
+      title='New Meeting'
+      description= 'Start an instant meeting'
+      handleClick = {() => setmeetingState('isInstantMeeting')}
+      className='bg-orange-500'
+       />
+      <HomeCard 
+      img='/icons/schedule.svg'
+      title='Schedule Meeting'
+      description= 'Schedule a meeting for later'
+      handleClick = {() => setmeetingState('isScheduleMeeting')}
+      className='bg-blue-500'
+      />
+      <HomeCard 
+      img='/icons/recordings.svg'
+      title='View Recordings'
+      description= 'check your past recordings'
+      handleClick = {() => router.push('/recordings')}
+      className='bg-purple-500'
+      />
+
+      <HomeCard 
+      img='/icons/join-meeting.svg'
+      title='Join Meeting'
+      description= 'via invitation link'
+      handleClick = {() => setmeetingState('isJoiningMeeting')}
+      className='bg-yellow-500'
+      />
+
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setmeetingState(undefined)}
+        title='Start an instant meeting'
+        buttonText="start Meeting"
+        className = 'text-center'
+        handleClick ={CreateMeeting}
+      />
+
+    </section>
+  )
+}
+
+export default MeetingTypeList
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image';
+import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className=' flex flex-between justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href='/' className='flex items-center gap-1' >
+        <Image
+        src='/icons/logo.svg'
+        width={32}
+        height={32}
+        alt="Zoom logo"
+        className='max-sm: size-10'
+        />
+        <p className='text-[26px] text-white font-extrabold max-sm:hidden'>Zoom</p>
+      </Link>
+      <div className='flex-between gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
+      </div>
+
+    </nav>
   )
 }
 
